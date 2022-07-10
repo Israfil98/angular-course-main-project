1161ebdec6ee2dc02572215853d5e8981da5a4f3
Israfil98 <israfil.guseynov.98@inbox.ru>
2022-07-10 20:52:33 +0300

    add navigation logic

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  @Output() selectedNavItem = new EventEmitter<string>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSelect(navItem: string) {
+    this.selectedNavItem.emit(navItem);
   }
-
 }
