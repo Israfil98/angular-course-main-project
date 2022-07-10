379211d53d0ae6779426deafa7bcebccfa9eba22
Israfil98 <israfil.guseynov.98@inbox.ru>
2022-07-10 21:06:35 +0300

    move recipe to a recipe-item component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,20 +1,32 @@
-import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from './../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A test Recipe', 'This is a test description', 'https://andyeklund.com/wp-content/uploads/2017/02/Recipes-Title.png'),
-    new Recipe('A test Recipe', 'This is a test description', 'https://andyeklund.com/wp-content/uploads/2017/02/Recipes-Title.png')
+    new Recipe(
+      'A test Recipe 1',
+      'This is a test description',
+      'https://andyeklund.com/wp-content/uploads/2017/02/Recipes-Title.png'
+    ),
+    new Recipe(
+      'A test Recipe 2',
+      'This is a test description',
+      'https://andyeklund.com/wp-content/uploads/2017/02/Recipes-Title.png'
+    ),
+    new Recipe(
+      'A test Recipe 3',
+      'This is a test description',
+      'https://andyeklund.com/wp-content/uploads/2017/02/Recipes-Title.png'
+    ),
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
