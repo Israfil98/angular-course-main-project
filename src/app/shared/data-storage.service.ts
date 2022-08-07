47c812792c9cea815737711c4033826a19a435ed
Israfil98 <israfil.guseynov.98@inbox.ru>
2022-08-07 12:33:47 +0400

    transforming response data with rxjs operator map

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
 import { Recipe } from './../recipes/recipe.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 import { RecipeService } from './../recipes/recipe.service';
 
@@ -24,6 +25,16 @@ export class DataStorageService {
     this.http
       .get<Recipe[]>(
         'https://angular-course-main-project-default-rtdb.europe-west1.firebasedatabase.app/recipes.json'
+      )
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        })
       )
       .subscribe((res) => {
         console.log(res);
