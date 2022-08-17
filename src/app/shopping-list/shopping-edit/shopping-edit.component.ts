5fb42cfa4159480e4ec43b0084d703e12348e72a
Israfil98 <israfil.guseynov.98@inbox.ru>
2022-08-17 16:14:48 +0400

    replace update and delete logic from service to ngrx state

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -6,6 +6,7 @@ import { NgForm } from '@angular/forms';
 import { Ingredient } from './../../shared/ingredient.model';
 import { ShoppingListService } from './../shopping-list.service';
 import * as ShoppingListActions from '../store/shopping-list.actions';
+import * as fromShoppingList from '../store/shopping-list.reducer';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -21,30 +22,48 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
 
   constructor(
     private shoppingListService: ShoppingListService,
-    private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>
+    private store: Store<fromShoppingList.AppState>
   ) {}
 
   ngOnInit(): void {
-    this.subscription = this.shoppingListService.startedEditing.subscribe(
-      (id: number) => {
-        this.isEditMode = true;
-        this.editingItemIndex = id;
-        this.editedItem = this.shoppingListService.getIngredient(id);
-        this.form.setValue({
-          name: this.editedItem.name,
-          amount: this.editedItem.amount,
-        });
-      }
-    );
+    this.subscription = this.store
+      .select('shoppingList')
+      .subscribe((stateData) => {
+        if (stateData.editedIngredientIndex !== null) {
+          this.isEditMode = true;
+          this.editedItem = stateData.editedIngredient;
+          this.form.setValue({
+            name: this.editedItem.name,
+            amount: this.editedItem.amount,
+          });
+        } else {
+          this.isEditMode = false;
+        }
+      });
+
+    // this.subscription = this.shoppingListService.startedEditing.subscribe(
+    //   (id: number) => {
+    //     this.isEditMode = true;
+    //     this.editingItemIndex = id;
+    //     this.editedItem = this.shoppingListService.getIngredient(id);
+    //     this.form.setValue({
+    //       name: this.editedItem.name,
+    //       amount: this.editedItem.amount,
+    //     });
+    //   }
+    // );
   }
 
   onSubmit(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     if (this.isEditMode) {
-      this.shoppingListService.updateIngredient(
-        this.editingItemIndex,
-        newIngredient
+      // this.shoppingListService.updateIngredient(
+      //   this.editingItemIndex,
+      //   newIngredient
+      // );
+      this.store.dispatch(
+        new ShoppingListActions.UpdateIngredient(newIngredient)
       );
     } else {
       // this.shoppingListService.addIngredient(newIngredient);
@@ -57,14 +76,17 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   onClear() {
     this.isEditMode = false;
     this.form.reset();
+    this.store.dispatch(new ShoppingListActions.StopEdit());
   }
 
   onDelete() {
-    this.shoppingListService.deleteIngredient(this.editingItemIndex);
+    // this.shoppingListService.deleteIngredient(this.editingItemIndex);
+    this.store.dispatch(new ShoppingListActions.DeleteIngredient());
     this.onClear();
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
+    this.store.dispatch(new ShoppingListActions.StopEdit());
   }
 }
--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,12 +1,24 @@
 import { Ingredient } from '../../shared/ingredient.model';
 import * as ShoppingListActions from './shopping-list.actions';
 
-const initialState = {
+export interface AppState {
+  shoppingList: ShoppingListState;
+}
+
+export interface ShoppingListState {
+  ingredients: Ingredient[];
+  editedIngredient: Ingredient | null;
+  editedIngredientIndex: number | null;
+}
+
+const initialState: ShoppingListState = {
   ingredients: [new Ingredient('Apples', 5), new Ingredient('Potatoes', 10)],
+  editedIngredient: null,
+  editedIngredientIndex: null,
 };
 
 export function shoppingListReducer(
-  state = initialState,
+  state: ShoppingListState = initialState,
   action: ShoppingListActions.ShoppingListActionTypes
 ) {
   switch (action.type) {
@@ -19,6 +31,42 @@ export function shoppingListReducer(
       return {
         ...state,
         ingredients: [...state.ingredients, ...action.payload],
+      };
+    case ShoppingListActions.UPDATE_INGREDIENT:
+      const ingredient = state.ingredients[state.editedIngredientIndex];
+      const updatedIngredient = {
+        ...ingredient,
+        ...action.payload,
+      };
+      const updatedIngredients = [...state.ingredients];
+      updatedIngredients[state.editedIngredientIndex] = updatedIngredient;
+
+      return {
+        ...state,
+        ingredients: updatedIngredients,
+        editedIngredient: null,
+        editedIngredientIndex: null,
+      };
+    case ShoppingListActions.DELETE_INGREDIENT:
+      return {
+        ...state,
+        ingredients: state.ingredients.filter(
+          (ingr, index) => index !== state.editedIngredientIndex
+        ),
+        editedIngredient: null,
+        editedIngredientIndex: null,
+      };
+    case ShoppingListActions.START_EDIT:
+      return {
+        ...state,
+        editedIngredient: { ...state.ingredients[action.payload] },
+        editedIngredientIndex: action.payload,
+      };
+    case ShoppingListActions.STOP_EDIT:
+      return {
+        ...state,
+        editedIngredient: null,
+        editedIngredientIndex: null,
       };
     default:
       return state;
